60a55debdd332855a24ecdaf74cc97a510a5c199
IMOKURI <nenegi.01mo@gmail.com>
2015-04-08 00:36:43 +0900

    Add base width info to css/default.hs

--- src/css/default.hs
+++ src/css/default.hs
@@ -1,8 +1,15 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Prelude hiding (div, (**))
+import Prelude hiding (all, div, (**))
 import Clay
+
+import qualified Clay.Media as M
+
+
+main :: IO ()
+main = putCss defaultStyle
+
 
 defaultStyle :: Css
 defaultStyle = do
@@ -48,14 +55,34 @@ defaultStyle = do
     fontStyle italic
 
   div # "#footer" ? do
-    borderTop    solid (px 2) black
-    color        "#555"
-    fontSize     (px 12)
-    marginTop    (px 30)
-    sym2 padding (px 12) nil
-    textAlign    (alignSide sideRight)
+    borderTop      solid (px 2) black
+    color          "#555"
+    fontSize       (px 12)
+    marginTop      (px 30)
+    sym2 padding   (px 12) nil
+    textAlign      (alignSide sideRight)
+    textTransform  uppercase
 
+-------------------------------------------------------
 
-main :: IO ()
-main = putCss defaultStyle
+unit, half :: Integer -> Size Abs
+unit = px . (* 24)
+half = px . (* 12)
+
+u1, u2, u3, u4 :: Size Abs
+u1 = unit 1
+u2 = unit 2
+u3 = unit 3
+u4 = unit 4
+
+pageWidth :: Size Abs
+pageWidth = unit 25
+
+narrow :: Css -> Css
+narrow = query all [M.maxWidth pageWidth]
+
+wide :: Css -> Css
+wide = query all [M.minWidth pageWidth]
+
+-------------------------------------------------------
 
