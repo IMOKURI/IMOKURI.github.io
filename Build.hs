da3300bd72e75482669db7457bd1f73f624a4417
IMOKURI <nenegi.01mo@gmail.com>
2016-09-26 18:33:03 +0900

    update build scripts

--- Build.hs
+++ Build.hs
@@ -6,24 +6,38 @@ import Development.Shake.Command
 import Development.Shake.FilePath
 
 main :: IO ()
-main = shakeArgs options $ do
-  phony "build" $ do
-    () <- cmd "stack" "build"
-    withTempDir $ \dir -> do
-      () <- cmd "mv" "_site/.git" dir
-      () <- cmd "stack" "exec" "site" "rebuild"
-      cmd "mv" (dir </> ".git") "_site/"
+main = shakeArgs shakeOptions $ do
+  phony "update" update
+
+  phony "build" build
 
   phony "watch" $
     cmd "stack" "exec" "site" "watch"
 
-  phony "reset" $ do
-    () <- cmd "stack" "exec" "site" "clean"
-    () <- cmd "git" "submodule" "init"
-    () <- cmd "git" "submodule" "update"
-    cmd "git" "submodule" "foreach" "git" "pull" "origin" "master"
+  phony "reset" $
+    cmd "git" "submodule" "foreach" "git" "reset" "--hard" "HEAD"
 
-options :: ShakeOptions
-options = shakeOptions
-  { shakeProgress = progressSimple
-  }
+  phony "sdiff" $
+    cmd (Cwd "_site") "git" "diff"
+
+  phony "clean" $
+    cmd "stack" "exec" "site" "clean"
+
+  priority 0 $ action build
+
+
+update :: Action ()
+update = do
+  () <- cmd "git" "pull"
+  () <- cmd "git" "submodule" "init"
+  () <- cmd "git" "submodule" "update"
+  () <- cmd "git" "submodule" "foreach" "git" "pull" "origin" "master"
+  cmd (Cwd "_site") "git" "checkout" "master"
+
+build :: Action ()
+build = do
+  () <- cmd "stack" "build"
+  withTempDir $ \dir -> do
+    () <- cmd "mv" "_site/.git" dir
+    () <- cmd "stack" "exec" "site" "rebuild"
+    cmd "mv" (dir </> ".git") "_site/"
