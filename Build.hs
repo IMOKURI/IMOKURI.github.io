854acf034411d20271d503e2d61877bc59d57ff2
IMOKURI <nenegi.01mo@gmail.com>
2016-10-19 04:59:45 +0900

    use lts-6.22

--- Build.hs
+++ Build.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-6.16 --install-ghc runghc --package shake
+-- stack --resolver lts-6.22 --install-ghc runghc --package shake
 
 import Development.Shake
 import Development.Shake.Command
